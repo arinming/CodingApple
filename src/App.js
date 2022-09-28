342d83a7b8a9cbe58466c98f3c80ba7752397213
아딘컴\김아린 <jyandysys1@naver.com>
2022-09-29 04:17:59 +0900

    feat: undate

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import Header from './Components/Header';
+import DayList from './Components/DayList';
+import Day from './Components/Day';
+import EmptyPage from './Components/EmptyPage';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Switch> 
+          <Route exact path="/">
+            <DayList />
+          </Route>
+          <Route path="/day/:day">
+            <Day />
+          </Route> 
+          <Route>
+            <EmptyPage />
+            </Route>    
+        </Switch>
+      </div>
+    </BrowserRouter>   
+  );
+}
+
+export default App;
---
+++ src/Components/UserName.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function UserName({name}) {
+    return <p>Hello, {name}</p>
+}
