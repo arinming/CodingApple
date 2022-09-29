b6d1f89ff81a349359e924c3097f571acecc49da
아딘컴\김아린 <jyandysys1@naver.com>
2022-09-30 05:14:32 +0900

    feat:공부

--- src/App.js
+++ src/App.js
@@ -1,47 +1,28 @@
-import React, { useState } from 'react';
-
-import logo from './logo.svg';
-import './App.css';
-
+import Header from './Components/Header';
+import DayList from './Components/DayList';
+import Day from './Components/Day';
+import EmptyPage from './Components/EmptyPage';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function App() {
-
-  let [이름, 이름변경] = useState(['김아린', 'arinming']);
-
-  let posts = '강남 고기 맛집';
-
   return (
-    <div className="App">
-      <div className="black-nav">
-        <nav>
-          <h1>봐라오케</h1>
-          <button>악보 리스트</button>
-          <button>악보 변환</button>
-          <button>악보 등록하기</button>
-          <button>로그아웃</button>
-        </nav>
-        
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Switch> 
+          <Route exact path="/">
+            <DayList />
+          </Route>
+          <Route path="/day/:day">
+            <Day />
+          </Route> 
+          <Route>
+            <EmptyPage />
+          </Route>
+        </Switch>
       </div>
-      <div>
-        <img
-         scr={`${process.env.PUBLIC_URL}/public/img/balaok.png`}
-         className='Web-logo'
-         alt='React'></img>
-      </div>
-      <div className="list">
-        <h3> { 이름[0] } </h3>
-        <h3> { 이름[1] } </h3>
-        <hr/>
-      </div>
-    </div>
-  )
-
-
-  function 함수(){
-    return 100
-  }
-
-  
+    </BrowserRouter>   
+  );
 }
 
 export default App;
